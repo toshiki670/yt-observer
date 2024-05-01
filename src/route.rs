222914f522650766d75a1a7b34aac461afc91dc5
Toshiki <bushy.trivial.0o@icloud.com>
2024-05-01 21:22:02 +0900

    Create layout

--- src/route.rs
+++ src/route.rs
@@ -1,30 +1,69 @@
 use yew::prelude::*;
-use yew_router::prelude::*;
+use yew_nested_router::prelude::{Router as YewRouter, *};
 
-#[derive(Clone, Routable, PartialEq)]
-enum Route {
-    #[at("/")]
-    Home,
-    #[at("/secure")]
-    Secure,
-    #[not_found]
-    #[at("/404")]
+use crate::page::Page;
+
+#[derive(Debug, Default, Clone, PartialEq, Eq, Target)]
+pub enum Route {
+    #[default]
+    Dashboard,
+    Channel,
     NotFound,
 }
 
-fn switch(routes: Route) -> Html {
+fn render(routes: Route) -> Html {
     match routes {
-        Route::Home => html! { <h1><Link<Route> to={Route::Secure}>{ "click here to go source" }</Link<Route>></h1> },
-        Route::Secure => html! { <h1><Link<Route> to={Route::Home}>{ "click here to go Home" }</Link<Route>></h1> },
+        Route::Dashboard => {
+            html! {<Page><h1>{ "Dashboard" }</h1></Page> }
+        }
+        Route::Channel => {
+            html! { <Page><h1>{ "Channel" }</h1></Page> }
+        }
         Route::NotFound => html! { <h1>{ "404" }</h1> },
     }
 }
 
 #[function_component(Router)]
 pub fn router() -> Html {
+
     html! {
-       <BrowserRouter>
-            <Switch<Route> render={switch} /> // <- must be child of <BrowserRouter>
-        </BrowserRouter>
+        <YewRouter<Route> default={Route::Dashboard}>
+            <Switch<Route> render={render} />
+        </YewRouter<Route>>
     }
 }
+
+
+
+
+
+// #[function_component(Router)]
+// pub fn router() -> Html {
+//     let sidebar = html_nested! {
+//         <PageSidebar>
+//             <Nav>
+//                 <NavList>
+//                     <NavRouterItem<Route> to={Route::Index}>{"Index"}</NavRouterItem<Route>>
+//                 </NavList>
+
+//             </Nav>
+//         </PageSidebar>
+//     };
+//     html! {
+//         <BrowserRouter>
+//             <Page sidebar>
+//                 <PageSection>
+//                     // <Switch<Route> render={switch} />
+//                         {"my content"}
+//                 </PageSection>
+
+//             </Page>
+
+//         </BrowserRouter>
+//     }
+// }
+
+// #[function_component(Sidebar)]
+// pub fn sidebar() -> Html {
+//     html! {<PageSidebar><Nav></Nav></PageSidebar>}
+// }
